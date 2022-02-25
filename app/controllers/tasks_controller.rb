3c4d8b7f83b97165ecd620fff06415c668ab9493
nobuo.watanabe <4743530+nobuwo@users.noreply.github.com>
2022-02-25 03:43:32 +0000

    create CRUD pages

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,67 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: %i[ show edit update destroy ]
+
+  # GET /tasks or /tasks.json
+  def index
+    @tasks = Task.all
+  end
+
+  # GET /tasks/1 or /tasks/1.json
+  def show
+  end
+
+  # GET /tasks/new
+  def new
+    @task = Task.new
+  end
+
+  # GET /tasks/1/edit
+  def edit
+  end
+
+  # POST /tasks or /tasks.json
+  def create
+    @task = Task.new(task_params)
+
+    respond_to do |format|
+      if @task.save
+        format.html { redirect_to task_url(@task), success: "Task が正常に投稿されました" }
+      else
+        flash.now[:danger] = 'Task が投稿されませんでした'
+        format.html { render :new }
+      end
+    end
+  end
+
+  # PATCH/PUT /tasks/1 or /tasks/1.json
+  def update
+    respond_to do |format|
+      if @task.update(task_params)
+        format.html { redirect_to task_url(@task), success: "Task が正常に更新されました." }
+      else
+        flash.now[:danger] = 'Task が更新されませんでした'
+        format.html { render :edit }
+      end
+    end
+  end
+
+  # DELETE /tasks/1 or /tasks/1.json
+  def destroy
+    @task.destroy
+
+    respond_to do |format|
+      format.html { redirect_to tasks_url, success: "Task は正常に削除されました" }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_task
+      @task = Task.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def task_params
+      params.require(:task).permit(:content)
+    end
+end
