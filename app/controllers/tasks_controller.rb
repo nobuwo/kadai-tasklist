3c8e09c2562f0df2f4fa6241827210ccd4fb0145
nobuo.watanabe <4743530+nobuwo@users.noreply.github.com>
2022-03-11 08:52:43 +0000

    modify task permission and delete users index and show

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,7 @@
 class TasksController < ApplicationController
   before_action :require_logged_in
-  before_action :set_task, only: %i[ show edit update destroy ]
+  before_action :correct_user, only: %i[show edit update destroy]
+  before_action :set_task, only: %i[show edit update destroy]
 
   # GET /tasks or /tasks.json
   def index
@@ -58,13 +59,21 @@ class TasksController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_task
-      @task = Task.find(params[:id])
-    end
+  
+  # Use callbacks to share common setup or constraints between actions.
+  def set_task
+    @task = Task.find(params[:id])
+  end
 
-    # Only allow a list of trusted parameters through.
-    def task_params
-      params.require(:task).permit(:content, :status)
+  # Only allow a list of trusted parameters through.
+  def task_params
+    params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
     end
+  end
 end
